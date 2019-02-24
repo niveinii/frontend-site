5a8369121f9cc5d16edb91f5be7fa6025dff618d
niveinii <44360733+niveinii@users.noreply.github.com>
2019-02-24 18:55:57 +0200

    added items amount + price

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,9 +12,33 @@ inventory=INVENTORYLIST;
 head=HEADER;
 searchTerm: string; 
 p:number=1;
-addPrice () {
+costSum:number= 0;
+numberOfItems:number=0;
+addPrice (a:any) {
+  if (a.checklist === "Add"){
+    this.costSum = a.cost + this.costSum;
+    a.checklist = "Remove"
+    this.numberOfItems =+ this.numberOfItems + 1;
+    a.color = "green";
+    console.log(this.costSum);
+    console.log(this.numberOfItems);
+
+
+  }
+  else if (a.checklist === "Remove"){
+    this.costSum =this.costSum - a.cost ;
+    a.checklist = "Add"
+    this.numberOfItems =+ this.numberOfItems - 1;
+    console.log(this.costSum);
+    console.log(this.numberOfItems);
+
+  }
+
+
+ 
 
 }
+
 
   constructor() { }
 
