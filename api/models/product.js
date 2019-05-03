5e5ed8d0da503c6239026d62bd3d8cf38a0879c1
niveinii <44360733+niveinii@users.noreply.github.com>
2019-05-03 06:50:21 +0300

    additional features
    Added product manager page (connected to the product list @ home
    Added login page with working backend
    fixed CSS & HTML
    Added JWT Infra

---
+++ api/models/product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose'),
+Schema = mongoose.Schema;
+
+// Defining a new mongoDB Schema for products
+let Product = new Schema({
+    picture: String,
+    description: String,
+    cost: Number,
+    addItem:String,
+    removeItem:String,
+    quantity:Number
+}, {collection: 'Product'});
+module.exports = mongoose.model('Product', Product);
